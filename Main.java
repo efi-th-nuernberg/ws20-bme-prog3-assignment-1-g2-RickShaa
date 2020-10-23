99447a182ed539588e054aca3f842cf365411fe2
repl.it user <replituser@example.com>
2020-10-23 09:46:02 +0000

    added first assignment

--- Main.java
+++ Main.java
@@ -1,5 +1,26 @@
 class Main {
-  public static void main(String[] args) {
-    System.out.println("Rick");
+
+  public static void main(String[] args){
+    zahlenfilter();
+  }
+
+
+  public static void zahlenfilter() {
+    
+    for(int i =1; i<=200; i++)
+    {
+      if(i%5 ==0)
+      {
+        System.out.println(i + " ZAHL ist durch 5 teilbar");
+      }
+      else if(i%10 ==9)
+      {
+        System.out.println(i +" ZAHL endet auf 9");
+      }
+      else if((((i-1)+i)%3) == 0)
+      {
+        System.out.println(i +" ZAHL und VORGÄNGER addiert ergeben ERGEBNIS und");
+      }
+    }
   }
 }
